86124d18da03ece9fa6ad4d65e3ddfd5a9f48434
strange-quark-oss <gautamsiddharth81@gmail.com>
2024-09-21 03:13:02 +0530

    Update Termnal.java

--- firstcode/src/terminal/Termnal.java
+++ firstcode/src/terminal/Termnal.java
@@ -1,9 +1,11 @@
 package terminal;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Termnal {
 	public static void main(String[]args) {
 	
 		System.out.println("Firstpage");
+		ChromeDriver driver = new ChromeDriver();
+		
 
  }
 }
